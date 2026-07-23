912cf8937d5e9387724d890cb72aafb415868bd5
Zak861 <uenluezak861@gmail.com>
2026-07-23 15:40:26 +0200

    Recursion Beispiel mit Quersumme

---
+++ recursionQuersumme.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int quersumme(int *Zahl){
+    if(Zahl < 10){
+        return *Zahl;
+    }
+    else{
+        return (*Zahl%10) + (quersumme(*Zahl/10));
+    }
+}
+int main(){
+    int Zahl = 132;
+
+    printf("%d", quersumme(Zahl));
+}
